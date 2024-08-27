65cb4a13b26d38c660ac6319e89704e58cfd905a
Renato Oliveira Fraga <rofraga74@gmail.com>
2024-08-26 21:19:18 -0500

    Firebase integration completed

--- Grocery/GroceryApp.swift
+++ Grocery/GroceryApp.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct GroceryApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+
+  init() {
+    FirebaseApp.configure()
+    print("Configured Firebase!")
+  }
+  var body: some Scene {
+    WindowGroup {
+      MainView()
     }
+  }
 }
