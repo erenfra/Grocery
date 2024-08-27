91a3b13da0adb9a9a03b6d4031c679779db15bdc
Renato Oliveira Fraga <rofraga74@gmail.com>
2024-08-26 20:10:15 -0500

    Initial Commit

---
+++ Grocery/GroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryApp.swift
+//  Grocery
+//
+//  Created by Renato Oliveira Fraga on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
